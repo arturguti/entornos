171a2f7a0b958d1762f1d13024898e73305dba36
Arturo <arturogutierrez2002marzo@gmail.com>
2022-04-02 11:31:49 +0200

    Primer commit

---
+++ src/paquete1/Main.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TrianguloEquilatero t1 = new TrianguloEquilatero(10, 5);
+		System.out.println(t1.calculaArea());
+		System.out.println(t1.calculaPerimetro());
+
+	}
+
+}
---
+++ src/paquete1/TrianguloEquilatero.java
@@ -0,0 +1,46 @@
+package paquete1;
+
+public class TrianguloEquilatero {
+	private double lado;
+	private double altura;
+	
+	TrianguloEquilatero (double lado, double altura){
+		this.lado = lado;
+		this.altura = altura;
+	}
+	
+	public double getLado() {
+		return lado;
+	}
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	double calculaArea() {
+		double area = 0;
+		if (altura==0){
+			throw new ArithmeticException("No es posible que la altura sea cero");
+		} else {
+			area = (lado * 2)/altura;
+		}
+		return area;
+	}
+	
+	double calculaPerimetro() {
+		double perimetro = lado*3;
+		return perimetro;
+	}
+	void metodoNoFuncional() {
+		
+	}
+
+}
